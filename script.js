5d26ef1a143d4f5cfaeb1c5311714ef6c39057b2
Katie Durga <katie.durga@gmail.com>
2020-01-09 15:01:16 -0500

    finally a working password generator

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ var lowerCase = document.getElementById("lowerCase");
 var upperCase = document.getElementById("upperCase");
 var specialChar = document.getElementById("specialChars");
 var passwordDisplay = document.getElementById("passwordDisplay");
-var inputLength=0;
+// var inputLength=0;
 
-var result = [];
 // var result = "";
 // lengthInput(passLength);
 
@@ -32,39 +31,43 @@ var charsets = {
 //grabs user input, verifies length requirement
 generateButton.addEventListener("click", function(e){
    e.preventDefault();
-   //grabs form value for password length
-   passwordDisplay.placeholder=generatePassword();
+   var passLength = document.getElementById("length").value;
+   lengthInput(passLength);
+   // passwordDisplay.placeholder=generatePassword();
 });
 
 
 // password length set
 function lengthInput(num) {
    if (num >= 8 && num <= 128) {
-      // getChars();
+      passwordDisplay.placeholder=generatePassword();
       return num
    }
    else {
-   alert("Please enter a number between 8 and 128")
+   alert("Please enter a number between 8 and 128");
+   passwordDisplay.placeholder="";
    return null
+   
    }
 }
 
 
 
 function getChars() {
+   var result = "";
    if (numbers.checked == true) {
-      result = result += charsets.numbers.charset;
+      result += charsets.numbers.charset;
    }
    if (upperCase.checked == true) {
-      result = result += charsets.upperCase.charset;
+      result += charsets.upperCase.charset;
       // console.log(result);
    }
    if (lowerCase.checked == true) {
-      result = result += charsets.lowerCase.charset;
+      result += charsets.lowerCase.charset;
       // console.log(result);
    }
    if (specialChar.checked == true) {
-      result = result += charsets.specialChar.charset;
+      result += charsets.specialChar.charset;
       // console.log(result);
    }
    console.log(result);
@@ -73,7 +76,8 @@ function getChars() {
 
 //chooses random integer inside length of charset array
 function randomInt(charsetLength) {
-	var x = Math.floor(Math.random() * charsetLength) + 1;
+   var x = Math.floor(Math.random() * charsetLength);
+   console.log(x);
 	if (x < 0 || x >= charsetLength) {
       alert ("Random integer not in index");
       return null;
@@ -87,7 +91,7 @@ function generatePassword() {
    var password = "";
    var passLength = document.getElementById("length").value;
    var charArray = getChars();
-
+   
    //if user doesn't choose any charset options
    if (charArray.length) {
       for (var i = 0; i < parseInt(passLength); i++){
@@ -106,6 +110,4 @@ function generatePassword() {
 function doCopy() {
    navigator.clipboard.writeText(generatedPassword);
 }
-
-
 
